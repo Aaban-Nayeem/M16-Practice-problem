1de4248362a87e48d13c8dd3e9b04b24944a9c2d
Aaban Nayeem <aabannayeem4358@gmail.com>
2023-07-29 20:16:35 +0600

    first commit

---
+++ problem-1.js
@@ -0,0 +1,9 @@
+//^ Harry's mom given money to buy orange Tk 1000
+var totalMoney = 1000;
+
+//^ 1 KG of orange and 1 KG of apple price is Tk 700
+var orangePrice = 700;
+
+//* The shopkeeper will back:
+
+console.log(totalMoney - orangePrice);
